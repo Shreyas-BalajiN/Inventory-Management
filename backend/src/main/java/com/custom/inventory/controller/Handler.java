35df469607ff8fc1f4c866087d272e9fa64045e5
Giri Sharan Reddy Pusuluru <giri.rdy1@gmail.com>
2023-07-27 11:43:59 -0300

    error handler

---
+++ backend/src/main/java/com/custom/inventory/controller/Handler.java
@@ -0,0 +1,37 @@
+package com.custom.inventory.controller;
+
+import com.custom.inventory.protocol.Response;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class Handler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> handleException(Exception ex) {
+        Response response = new Response(List.of(), HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.name());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<Response> handleMethodNotAllowed(HttpRequestMethodNotSupportedException ex) {
+        Response response = new Response(List.of(),HttpStatus.METHOD_NOT_ALLOWED.value(), HttpStatus.METHOD_NOT_ALLOWED.name());
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(response);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Response> handleBadRequest(HttpMessageNotReadableException ex) {
+        Response response = new Response(List.of(), HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.name());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+}
