ced214dd240185ef58ab2a3b91fb31be572da4c9
Aniketh Kazi <an584274@dal.ca>
2023-07-27 21:58:25 -0300

    Adding Store model

---
+++ backend/src/main/java/com/custom/inventory/model/Store.java
@@ -0,0 +1,56 @@
+package com.custom.inventory.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "stores")
+@CompoundIndexes({
+        @CompoundIndex(name = "store_name_email_index", def = "{'name': 1, 'email': 1}", unique = true)
+})
+public class Store {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private List<Zone> zones;
+
+    public Store(String name, String email, List<Zone> zones) {
+        this.name = name;
+        this.email = email;
+        this.zones = zones;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Zone> getZones() {
+        return zones;
+    }
+
+}
